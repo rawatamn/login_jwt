9397ea86c2c9dbb8a7179d58a8783d1f701770c3
rawatamn <amanrawatwins@gmail.com>
2025-03-24 15:38:17 +0530

    changing movie model

--- backend/models/movie.js
+++ backend/models/movie.js
@@ -1,11 +1,4 @@
-import mongoose from "mongoose";
-import crypto from "crypto"; // Import crypto for unique ID generation
-
 const movieSchema = new mongoose.Schema({
-  _id: { 
-    type: String, 
-    default: () => crypto.randomBytes(12).toString("hex"), // ✅ 24-character ID
-  },
   title: { type: String, required: true },
   overview: { type: String, required: true },
   image: { type: String, required: true },
@@ -15,6 +8,7 @@ const movieSchema = new mongoose.Schema({
   rating: { type: Number, min: 0, max: 10, required: true }
 });
 
+// ✅ MongoDB will automatically generate an ObjectId
 const Movie = mongoose.model("Movie", movieSchema);
 
 export default Movie;
